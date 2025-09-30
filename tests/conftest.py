bbb869473aa3d5a2dc87f2133a31ec1a77a76dbb
David M. Raker <david.raker@pnnl.gov>
2025-09-29 19:27:48 -0700

    Updated test which communicates with a bacnet device to work with current interface, proxy, and testing code.

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,3 +1,12 @@
 """Configuration for the pytest test suite."""
 
-from volttrontesting.fixtures.volttron_platform_fixtures import *
+#from volttrontesting.fixtures.volttron_platform_fixtures import *
+
+# Sets command-line arguments which can be passed to pytest. Defaulting to None allows some tests to run without them.
+#  Tests which require these arguments should check for that they are not None and then skip or err.
+def pytest_addoption(parser):
+    parser.addoption("--device-config-file", action = "store", default=None,
+                     help = "Filename of the device configuration for the BACnet device being tested.")
+    parser.addoption("--registry-config-file", action = "store", default=None,
+                     help = "Filename of the registry configuration for the BACnet device being tested."
+                            "This is not required if registry is included as json in --device-config-file.")
